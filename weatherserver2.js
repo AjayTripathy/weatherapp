904518ed3bdcf976cb6664989d7513e70ea824a0
Fred Meyer <dungfu3@gmail.com>
2011-04-19 21:06:46 -0700

    added effectiveTemp()

--- weatherserver2.js
+++ weatherserver2.js
@@ -53,9 +53,29 @@ function end(type, data, res, jsonp){
 }
 
 function effectiveTemp(temp, windSpeed, humidity) {
-	
+	var finalTemp;
+	var c1 = -42.379;
+	var c2 = 2.014901523;
+	var c3 = 10.14333127;
+	var c4 = -0.22475541;
+	var c5 = -0.00683783;
+	var c6 = -0.05481717;
+	var c7 = 0.00122874;
+	var c8 = 0.00085282;
+	var c9 = -0.00000199;
+	if (temp < 50) {
+		finalTemp = 35.74 + (0.6215 * temp) - (35.75 * pow(windSpeed, 0.16)) + (0.4275 * pow(windSpeed, 0.16));
+	} else {
+		finalTemp = c1 + c2 * temp + c3 * humidity +
+					c4 * temp * humidity + c5 * pow(temp, 2) +
+					c6 * pow(humidity, 2) +
+					c7 * pow(temp, 2) * humidity + 
+					c8 * temp * pow(humidity, 2) + 
+					c9 * pow(temp, 2) * pow(humidity, 2);
+	}
+	return finalTemp;
 }
 
 
 var server = http.createServer(handleHttp);
-server.listen(9002);
+server.listen(9001);
